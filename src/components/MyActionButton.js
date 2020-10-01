d4738461a335b18a6f236a13e7ac295d8ed5d75f
Albert <albert.mensahansah@gmail.com>
2020-10-01 04:48:28 +0000

    yep

---
+++ src/components/MyActionButton.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default MyActionButton = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.fab}>
+      <TouchableOpacity onPress={() => navigation.navigate("qr_code_scanner")}>
+        <Ionicons
+          name="md-qr-scanner"
+          size={24}
+          color="white"
+          style={styles.actionButtonIcon}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  fab: {
+    position: "absolute",
+    width: 56,
+    height: 56,
+    alignItems: "center",
+    justifyContent: "center",
+    right: 20,
+    bottom: 20,
+    backgroundColor: "rgba(231,76,60,1)",
+    borderRadius: 30,
+    elevation: 10,
+  },
+  fabIcon: {
+    fontSize: 40,
+    color: "white",
+  },
+});
+
+//export default MyActionButton;
